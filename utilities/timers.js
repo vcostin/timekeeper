e9d228d815ef065b4468fc4a1242a128548355b0
Vadim Costin <vadim.costin@gmail.com>
2025-01-22 21:32:54 +0200

    feat: add updateTimer function to update existing timers based on scheduleId and datePurpose

--- utilities/timers.js
+++ utilities/timers.js
@@ -1,7 +1,7 @@
 const timers = new Map();
 
 // Function to set a timer for a specific schedule
-function setTimer(scheduleId, targetDate, callback) {
+function setTimer(scheduleId, targetDate, datePurpose, callback) {
   const currentDate = new Date();
   const timeDifference = targetDate - currentDate;
 
@@ -11,7 +11,7 @@ function setTimer(scheduleId, targetDate, callback) {
     if (!timers.has(scheduleId)) {
       timers.set(scheduleId, []);
     }
-    timers.get(scheduleId).push(timerId);
+    timers.get(scheduleId).push({ timerId, datePurpose });
   } else {
     console.log(`The target date for schedule ${scheduleId} is in the past!`);
   }
@@ -20,31 +20,58 @@ function setTimer(scheduleId, targetDate, callback) {
 // Function to clear all timers for a specific schedule
 function clearTimers(scheduleId) {
   if (timers.has(scheduleId)) {
-    timers.get(scheduleId).forEach(clearTimeout);
+    timers.get(scheduleId).forEach(({ timerId }) => clearTimeout(timerId));
     timers.delete(scheduleId);
   }
 }
 
 // Function to clear all existing timers
 function clearAll() {
-  timers.forEach((timerIds, _scheduleId) => {
-    timerIds.forEach(clearTimeout);
+  timers.forEach((timerInfos, _scheduleId) => {
+    timerInfos.forEach(({ timerId }) => clearTimeout(timerId));
   });
   timers.clear();
+}
+
+// Function to update a timer for a specific schedule and date purpose
+function updateTimer(scheduleId, targetDate, datePurpose, callback) {
+  if (timers.has(scheduleId)) {
+    const timerInfos = timers.get(scheduleId);
+    const timerInfoIndex = timerInfos.findIndex((info) =>
+      info.datePurpose === datePurpose
+    );
+
+    if (timerInfoIndex !== -1) {
+      clearTimeout(timerInfos[timerInfoIndex].timerId);
+      timerInfos.splice(timerInfoIndex, 1);
+    }
+
+    setTimer(scheduleId, targetDate, datePurpose, callback);
+  } else {
+    setTimer(scheduleId, targetDate, datePurpose, callback);
+  }
 }
 
 // Example usage
 // const scheduleId1 = "schedule1";
 // const scheduleId2 = "schedule2";
 
-// setTimer(scheduleId1, new Date("2025-01-15T10:00:00"), () => {
-//   console.log(`The specific time for schedule ${scheduleId1} has arrived!`);
+// setTimer(scheduleId1, new Date("2025-01-15T10:00:00"), "openTime", () => {
+//   console.log(`The open time for schedule ${scheduleId1} has arrived!`);
 // });
-// setTimer(scheduleId2, new Date("2025-01-16T10:00:00"), () => {
-//   console.log(`The specific time for schedule ${scheduleId2} has arrived!`);
+// setTimer(scheduleId2, new Date("2025-01-16T10:00:00"), "closeTime", () => {
+//   console.log(`The close time for schedule ${scheduleId2} has arrived!`);
+// });
+
+// To update a timer for a specific schedule and date purpose
+// updateTimer(scheduleId1, new Date("2025-01-15T12:00:00"), "openTime", () => {
+//   console.log(`The updated open time for schedule ${scheduleId1} has arrived!`);
 // });
 
 // To clear timers for a specific schedule
 // clearTimers(scheduleId1);
 
-export { clearTimers, setTimer, clearAll, timers };
+// To clear all timers
+// clearAll();
+
+export { clearAll, clearTimers, setTimer, timers, updateTimer };
--- utilities/timers.test.js
+++ utilities/timers.test.js
@@ -1,4 +1,10 @@
-import { clearTimers, setTimer, clearAll, timers } from "./timers.js";
+import {
+  clearAll,
+  clearTimers,
+  setTimer,
+  timers,
+  updateTimer,
+} from "./timers.js";
 import { assertSpyCalls, spy } from "@std/testing/mock";
 import { FakeTime } from "@std/testing/time";
 
@@ -8,7 +14,7 @@ Deno.test("should set a timer for a specific schedule", () => {
   const targetDate = new Date(Date.now() + 1000); // 1 second in the future
   const callback = () => {};
 
-  setTimer(scheduleId, targetDate, callback);
+  setTimer(scheduleId, targetDate, "openTime", callback);
 
   if (!timers.has(scheduleId)) {
     throw new Error("Timer was not set");
@@ -26,7 +32,7 @@ Deno.test("should not set a timer for a past date", () => {
   const targetDate = new Date(Date.now() - 1000); // 1 second in the past
   const callback = () => {};
 
-  setTimer(scheduleId, targetDate, callback);
+  setTimer(scheduleId, targetDate, "closeTime", callback);
 
   if (timers.has(scheduleId)) {
     throw new Error("Timer should not be set for a past date");
@@ -41,7 +47,7 @@ Deno.test("should clear all timers for a specific schedule", () => {
   const targetDate = new Date(Date.now() + 1000); // 1 second in the future
   const callback = () => {};
 
-  setTimer(scheduleId, targetDate, callback);
+  setTimer(scheduleId, targetDate, "openTime", callback);
   clearTimers(scheduleId);
 
   if (timers.has(scheduleId)) {
@@ -57,11 +63,35 @@ Deno.test("should execute the function at the specific time", () => {
   const targetDate = new Date(Date.now() + 1000); // 1 second in the future
   const executeSpy = spy(() => {});
 
-  setTimer(scheduleId, targetDate, executeSpy);
+  setTimer(scheduleId, targetDate, "openTime", executeSpy);
 
   time.tick(1000);
 
   assertSpyCalls(executeSpy, 1);
+
+  clearAll();
+  time.restore();
+});
+
+Deno.test("should update an existing timer for a specific schedule and date purpose", () => {
+  const time = new FakeTime();
+  const scheduleId = "schedule5";
+  const targetDate1 = new Date(Date.now() + 1000); // 1 second in the future
+  const targetDate2 = new Date(Date.now() + 2000); // 2 seconds in the future
+  const executeSpy1 = spy(() => {});
+  const executeSpy2 = spy(() => {});
+
+  setTimer(scheduleId, targetDate1, "openTime", executeSpy1);
+  updateTimer(scheduleId, targetDate2, "openTime", executeSpy2);
+
+  time.tick(1000);
+
+  assertSpyCalls(executeSpy1, 0); // The first timer should be cleared
+
+  time.tick(1000); // Ensure it waits for at least 2 seconds
+
+  assertSpyCalls(executeSpy2, 1); // The second timer should be executed
+
   clearAll();
   time.restore();
 });
